92754f131237250e3ae3e3ff457dd8ec49df1289
MarcosCS2004 <marcoscsfp2004@gmail.com>
2024-04-25 18:13:27 +0200

    Confirmar tarea Backend

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-# engine = create_engine("mysql+pymysql://root:@localhost:3306/tfg")
+#engine = create_engine("mysql+pymysql://root:@localhost:3306/tfg")
 engine = create_engine("mysql+pymysql://miguel_pinan:palomeras98@db4free.net/bdd_miguelp")
 
 meta = MetaData()
