5f797548013d4691c81f40011c45410ed0f4a256
miguel <miguelpi1998@gmail.com>
2024-05-15 00:28:43 +0200

    entrenamiento

--- Backend/routes/entrenamiento.py
+++ Backend/routes/entrenamiento.py
@@ -5,7 +5,7 @@ from schemas.entrenamientos import Entrenamientos  # Importa el esquema para cre
 
 ruta_entrenamientos = APIRouter()  # Cambia el nombre de la variable a sesiones_router
 
-@ruta_entrenamientos.get("/entrenamientos/sesion/{id}")
+@ruta_entrenamientos.get("/entrenamientos/rutina/{id}")
 def get_entrenamientos_by_sesion_id(id: int):
     query = conn.execute(entrenamientos.select().where(entrenamientos.c.id_rutinas == id))
     entrenamientos_list = []
@@ -20,12 +20,12 @@ def get_entrenamientos_by_sesion_id(id: int):
         entrenamientos_list.append(entrenamientos_dict)
     return entrenamientos_list
 
-@ruta_entrenamientos.post("/entrenamientos/sesion/{id}")
-def create_entrenamiento_for_sesion(id:int,  nombre: str = Form(...),dia_semana: str = Form(...)):
+@ruta_entrenamientos.post("/entrenamientos/rutina")
+def create_entrenamiento_for_sesion(id_rutina:int= Form(...),  nombre: str = Form(...),dia_semana: str = Form(...)):
     nuevo_entrenamiento = {
         "nombre": nombre,      
         "dia_semana":dia_semana,
-        "id_rutina": id
+        "id_rutina": id_rutina
     }
     conn.execute(entrenamientos.insert().values(nuevo_entrenamiento))
     conn.commit()
