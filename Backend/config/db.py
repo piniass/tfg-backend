12b1ad98234db76ac04783efa0cef2d9be27e155
miguel <miguelpi1998@gmail.com>
2024-05-16 21:38:55 +0200

    Update db.py

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/tfg")
-# engine = create_engine("mysql+pymysql://miguel_pinan:palomeras98@db4free.net/bdd_miguelp")
+# engine = create_engine("mysql+pymysql://root:@localhost:3306/tfg")
+engine = create_engine("mysql+pymysql://miguel_pinan:palomeras98@db4free.net/bdd_miguelp")
 
 meta = MetaData()
 conn = engine.connect()
