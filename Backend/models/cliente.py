affe36f3af341759f2bf2fce11815be52888f8c1
MarcosCS2004 <marcoscsfp2004@gmail.com>
2024-05-26 17:26:55 +0200

    Solucion ondelete set null

--- Backend/models/cliente.py
+++ Backend/models/cliente.py
@@ -11,6 +11,6 @@ clientes = Table(
     Column("patologias", String(255), nullable=True),
     Column("avatar", String(100), nullable=True),
     Column("id_entrenador", Integer, ForeignKey("entrenadores.id"), nullable=True),
-    Column("id_rutina", Integer, ForeignKey("rutinas.id"), nullable=True)
+    Column("id_rutina", Integer, ForeignKey("rutinas.id", ondelete="SET NULL"), nullable=True)
 )
 
