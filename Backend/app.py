633f64ac63d8e0980bf42295a79173734319967b
miguel <miguelpi1998@gmail.com>
2024-05-30 18:37:01 +0200

    .

--- Backend/app.py
+++ Backend/app.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Configuración del CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:8000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:8000","https://conexionfit.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
